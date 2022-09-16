92dc1cdae687237ed873e78c37fc75c91e053651
kingmaulana <54925249+kingmaulana@users.noreply.github.com>
2022-09-16 08:17:33 +0700

    Initial Commit

---
+++ my-listing-app/my_listing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_listing_appApp.swift
+//  my-listing-app
+//
+//  Created by Lana Mac on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct my_listing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
